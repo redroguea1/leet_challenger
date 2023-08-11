eec1745b9cb001fc7fbdad3f4e8276f23a737d2a
redroguea1 <macdickerson@gmail.com>
2023-08-10 21:02:42 -0700

    239 sliding window maximium 2nd attempt, passing 31/57 test cases

--- 239_sliding_window_maximum.js
+++ 239_sliding_window_maximum.js
@@ -4,7 +4,7 @@
  * 
  * example
  * Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
- * Output: [3,3,5,5,6,7]
+ * Output: [3,3,5,5,6,7]`
  * @return {number[]}
  */
 var maxSlidingWindow = function(nums, k) {
@@ -12,11 +12,12 @@ var maxSlidingWindow = function(nums, k) {
     let r = k-1 // 3-1 = 2
     let output = []
     
+    //version 1: failed on 37/51 test cases. 
     while(r<=nums.length){
         let baby = nums.slice(l,r+1)
-        console.log("baby array in the corner: " + baby)
+        //console.log("baby array in the corner: " + baby)
         let max_num = baby.reduce((a,b) => Math.max(a,b))
-        console.log("max_num: " + max_num)
+        //console.log("max_num: " + max_num)
         output.push(max_num)
         l++
         r++
