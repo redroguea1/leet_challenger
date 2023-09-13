d5937fba288c215bd6e13668a6f7eb866c7fece1
redroguea1 <macdickerson@gmail.com>
2023-09-13 12:45:51 -0700

    141 code works with 2 pointer method in version 2

--- 141_linked_list_cycle.js
+++ 141_linked_list_cycle.js
@@ -11,21 +11,41 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
+    /* version 2 think about using a slow and fast pointer in order to create a loop, while condition will be fast and fast.next */
+    let slow = head;
+    let fast = head;
+    
+    while(fast && fast.next){
+        slow = slow.next;
+        fast = fast.next.next;
+        if(slow==fast) {
+            return true
+        }
+    }
+    return false
+
+
 
     /*Version 1 leveraging the pointer and creating a set to identify if a value has been hit before */
     //using a set check to see if that value has been hit before 
     //OR if pointer is head then return true else
     
-    if(head == null) return false
-    else if (head.next == null) return false 
-    let pointer = head.next
-    let set = new Set()
+ //using a set check to see if that value has been hit before 
+    //OR if pointer is head then return true else
+    let pointer;
+    if (head && head.next) pointer = head.next
+    else return false
     
+    let set = new Set();
+    let loop = 0;
     while(pointer!=null){
         //off case when we run back into our head pointer return true
-        if(pointer == head) return true;
+        console.log("loop is: " + loop);
+        loop++;
+        //if(pointer == head) return true;
         
-        //add to our set and then create 
+        //add to our set and then create
+        
         if(!set.has(pointer.val)) {
             set.add(pointer.val);
         }
