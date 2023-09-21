e054ea06cf0aa2cd0f397a3d8e73ad244e576797
redroguea1 <macdickerson@gmail.com>
2023-09-20 17:25:59 -0700

    1598 crawler solution 1st version using brute force and a counter

---
+++ 1598_crawler_log_folder.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} logs
+ * @return {number}
+ */
+var minOperations = function(logs) {
+    //if "./" - stay where you are = 0
+      //if "../" - go up1 = -1
+      //if x/ - go to that directory +1
+
+      //version 1, brute force solution using a counter that increments and decrement before it is returned
+      let moves = 0;
+      let i = 0;
+
+      while (i<logs.length){
+        if (logs[i] == "./"); //nothing happens
+        else if (logs[i] == "../" && moves != 0) moves--;
+          //need a rule here for when we have a file folder. 
+        else if(!logs[i].contains("."))) moves++
+        i++
+      }
+      return moves
+    
+};
--- 287_find_duplicate_num.js
+++ 287_find_duplicate_num.js
@@ -5,7 +5,21 @@
 var findDuplicate = function(nums) {
         /* version 2 requires Floyd's algorithm where you use two pointers to reference the various indices since each value in the array is pointing back to the array slot. Drawing this out ois the easiest way tot see this visually 
         but otherwise you will have 3 pointers slow, slow2, fast. after aligning slow and fast you use slow2 to identify the final value in a 2nd while loop  */
+        let slow, slow2, fast = 0; // declaration and assignment of multiple variables. 
 
+        //confirming that there is a loop and moving both pointer to that loop 
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) break;
+        }
+
+        //slow2 is moving through the loop at some point they will cross paths since slow will eventually loop back around to where slow2 is apporaching which is what we return the location of the duplicate. 
+        while(true) {
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if (slow == slow2) return slow
+        }
         /* version 1 use a set to identify the non duplicate numbers not sure if this meets the constant extra space requirement but does not meet the constant extra space requirement*/
         let set = new Set();
     
