92ace2fc7dcd6122f31b104c8fe80e030137f1bf
redroguea1 <macdickerson@gmail.com>
2023-09-27 09:50:05 -0700

    version 2 of Leetcode 23, getting a memeory error in leet code when running my new code. Going to rewrite in a simpler fashion and try again

--- 23_Merge_k_sorted_lists.js
+++ 23_Merge_k_sorted_lists.js
@@ -10,7 +10,44 @@
  * @return {ListNode}
  */
 var mergeKLists = function(lists) {
-    
+    //version 2 breaking up the problem 1) sorting through k list and 2) fuzing two list in a separate function. This way we can handle our various list with greater ease. 
+    if(!lists || lists.length ==0) return null;
+
+    while(lists.length > 1 ){
+        let finalList = [];
+
+        for(let i = 0; i<lists.length; i+2){
+            let list1 = lists[i];
+            let list2 = null;
+            if(lists[i+1]){
+                let list2 = lists[i+1]
+            }
+            finalList.push(mergeLists(list1, list2))
+        }
+        lists = finalList;
+    }
+    return lists[0]
+
+    var mergeLists = function(list1, list2){
+        let dummy = new ListNode();
+        let tail = dummy;
+        
+        while(list1 && list2){
+            if(list1.val<list2.val){
+                tail.next = list1;
+                list1 = list1.next;
+            }
+            else {
+                tail.next = list2;
+                list2 = list2.next;
+            }
+            tail = tail.next
+        }
+        if(list1) tail.next = list1;
+        else if(list2)tail.next = list2;
+
+        return dummy.next;
+    }
     
     //version1 
     //idea would be to use a similar approach to merging 2 linked list but in this case base it around an index, i, where we are merging lists[i] and list[i+1] until we have merged k lists...?
