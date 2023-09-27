eb06134394a2550cec9bad5951f114b193c05a0c
redroguea1 <macdickerson@gmail.com>
2023-09-27 10:59:35 -0700

    version 3 uses the simple pointer method to solve leetcode#23. use the pointer to iterate over throught lists and merge them using our merge of two sorts in order to create a final list and return it.

--- 23_Merge_k_sorted_lists.js
+++ 23_Merge_k_sorted_lists.js
@@ -10,25 +10,35 @@
  * @return {ListNode}
  */
 var mergeKLists = function(lists) {
-    //version 2 breaking up the problem 1) sorting through k list and 2) fuzing two list in a separate function. This way we can handle our various list with greater ease. 
-    if(!lists || lists.length ==0) return null;
 
-    while(lists.length > 1 ){
-        let finalList = [];
-
-        for(let i = 0; i<lists.length; i+2){
-            let list1 = lists[i];
-            let list2 = null;
-            if(lists[i+1]){
-                let list2 = lists[i+1]
-            }
-            finalList.push(mergeLists(list1, list2))
-        }
-        lists = finalList;
+    //version 3 using a simple pointer, iterate through each of the lists and merge them with our simple pointer. convering a simple pointer into a list and then merging to the list fromt here
+    let primal = null;
+    
+    for(let i=0; i<lists.length; i++){
+        primal = mergeLists(primal, lists[i])
     }
-    return lists[0]
+    
+    return primal;
 
-    var mergeLists = function(list1, list2){
+    //version 2 breaking up the problem 1) sorting through k list and 2) fuzing two list in a separate function. This way we can handle our various list with greater ease. 
+    // if(!lists || lists.length ==0) return null;
+
+    // while(lists.length > 1 ){
+    //     let finalList = [];
+
+    //     for(let i = 0; i<lists.length; i+2){
+    //         let list1 = lists[i];
+    //         let list2 = null;
+    //         if(lists[i+1]){
+    //             let list2 = lists[i+1]
+    //         }
+    //         finalList.push(mergeLists(list1, list2))
+    //     }
+    //     lists = finalList;
+    // }
+    // return lists[0]
+
+    function mergeLists(list1, list2){
         let dummy = new ListNode();
         let tail = dummy;
         
@@ -49,33 +59,33 @@ var mergeKLists = function(lists) {
         return dummy.next;
     }
     
-    //version1 
-    //idea would be to use a similar approach to merging 2 linked list but in this case base it around an index, i, where we are merging lists[i] and list[i+1] until we have merged k lists...?
-    if (lists == []) return lists
-    let dummy = new ListNode();
-    let tail = dummy;
-    let counter = 0
+    // //version1 
+    // //idea would be to use a similar approach to merging 2 linked list but in this case base it around an index, i, where we are merging lists[i] and list[i+1] until we have merged k lists...?
+    // if (lists == []) return lists
+    // let dummy = new ListNode();
+    // let tail = dummy;
+    // let counter = 0
     
-    for(let i= 0; i<lists.length; i++){
-        //console.log(lists[i]);
-        while(lists[i] && lists[i+1]){
-            if(lists[i].val< lists[i+1]){
-                tail.next = lists[i];
-                lists[i] = lists[i].next;
-            }
-            else {
-                tail.next = lists[i+1];
-                lists[i+1] = lists[i+1].next;
-            }
-            tail = tail.next;
-            console.log(counter + ":" );
-            console.log(lists[i].next);
-            counter++
-        }
-        if (lists[i]) tail.next = lists[i]
-        else if(lists[i+1]) tail.next = lists[i+1]
-        counter = 0;
-    }
-    return dummy.next;
+    // for(let i= 0; i<lists.length; i++){
+    //     //console.log(lists[i]);
+    //     while(lists[i] && lists[i+1]){
+    //         if(lists[i].val< lists[i+1]){
+    //             tail.next = lists[i];
+    //             lists[i] = lists[i].next;
+    //         }
+    //         else {
+    //             tail.next = lists[i+1];
+    //             lists[i+1] = lists[i+1].next;
+    //         }
+    //         tail = tail.next;
+    //         console.log(counter + ":" );
+    //         console.log(lists[i].next);
+    //         counter++
+    //     }
+    //     if (lists[i]) tail.next = lists[i]
+    //     else if(lists[i+1]) tail.next = lists[i+1]
+    //     counter = 0;
+    // }
+    // return dummy.next;
     
 };
