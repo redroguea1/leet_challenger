83f493f7f1a020f5df7a3c2521dfeecab83ce0aa
redroguea1 <macdickerson@gmail.com>
2023-11-21 10:40:34 -0800

    version 2 of 238 didn't work using multiple functions. pivoting to the video solution.

--- 238.product_of_array.js
+++ 238.product_of_array.js
@@ -1,4 +1,53 @@
 var productExceptSelf = function(nums) {
+    //version 2: made things into functions but still wasn't getting the right output. 
+      //if we have i we need all of nums except nums[i]
+    //insert product into output array
+    //needs to run in 0(n)
+    
+    //if i sue a mapi potentially add to it very quickly
+    //product function that takes values and adds them together
+    
+    let output = [];
+
+    
+    for (let num of nums){
+        //slice I want to use to identify subsections of the array
+        // num = 0, 1, 2, ... n 
+        let idx = nums.indexOf(num)
+        let shallow = newArr(nums, num)
+        //insert back into output array at idx
+        let total = productMaker(shallow);
+        console.log("total: " + total);
+        output.shift(total)
+    }
+    return output; 
+    
+    function newArr(arr, idx){
+        //remove the value and return a new array to be used. 
+        let shallow;
+        if(idx == 0){ //start
+            shallow = arr.slice(1, arr.length);
+            productMaker(shallow)
+        } else if (idx ==arr.length-1){ //end
+            shallow = arr.slice(0, idx);
+        } else { //middle 
+            let left = arr.slice(0,idx);
+            let right = arr.slice(idx+1, arr.length-1);
+            shallow = left.concat(right) 
+            //shallow = nums.slice(0,idx).concast(nums.slice(idx+1, nums.length-1))
+            // use the concat function to combine two arrays
+        }
+        return shallow
+    }
+    function productMaker(arr){ //computes
+        //(a*b)
+        let prod = 1
+        for(let num of arr){
+            prod = num*prod
+        }
+        return prod
+    }
+
     //version 1 
     //if we have i we need all of nums except nums[i]
     //insert product into output array
